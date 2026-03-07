bddd0a5ca2fef94d66f9ac4a7bc2299de1647a5d
Benjamin Wang <ben.wang9000@gmail.com>
2026-03-07 17:58:05 +0800

    feat: initial commit

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-import "./styles.css";
+import "./globals.css";
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
